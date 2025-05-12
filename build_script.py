438b953d6b1f73a229d15502dee300a297f54eec
HeydoubleU <56705510+HeydoubleU@users.noreply.github.com>
2025-05-12 14:31:18 -0500

    Updated for Bifrost 2.14

--- build_script.py
+++ build_script.py
@@ -1,45 +1,51 @@
-import os, shutil
-
-source_path = os.path.expanduser("~") + "/source"
-compounds_path = f"{source_path}/repos/HeydoubleU/Hyuu/packs"
-source_folder = "Hyuu"
-lib_name = "Hyuu"
+import os, shutil, re
 
 
-def removeFolder(rel_path):
-    root_path = f"{source_path}/{rel_path}"
-    if not os.path.isdir(root_path):
-        return
+def getProjectName(cmake_filename):
+    if not os.path.exists(cmake_filename):
+        raise FileNotFoundError(f"File {cmake_filename} does not exist")
 
-    for f in os.listdir(root_path):
-        p = os.path.join(root_path, f)
-        if (f.startswith(source_folder) or f.startswith(lib_name)) and os.path.isdir(p):
-            shutil.rmtree(p)
-            return
+    with open(cmake_filename, "r") as file:
+        content = file.read()
 
+    # Find the project name using regex
+    match = re.search(r"project\s*\(\s*([^\s]+)", content)
+    if not match:
+        raise ValueError("Project name not found in CMakeLists.txt")
+    
+    project_name = match.group(1)
+    print(f"Project name found: {project_name}")
+    return project_name
+
+
+root = os.path.dirname(__file__).replace("\\", "/")
+project_name = getProjectName(root + "/CMakeLists.txt")
 
 # remove previous build
-shutil.rmtree(f"{source_path}/repos/HeydoubleU/{source_folder}/out", ignore_errors=True)
-removeFolder("bifrost_libs")
-removeFolder("builds")
+if os.path.exists(root + "/build"):
+    shutil.rmtree(root + "/build")
 
-# run batch file
-os.system(f"{source_path}/repos/HeydoubleU/{source_folder}/build.bat")
 
-# copy files
-for folder in os.listdir(f"{source_path}/bifrost_libs"):
-    if not folder.startswith(lib_name):
+# run cmake
+os.chdir(root)
+os.system("cmake --preset windows")
+os.system("cmake --build --preset windows")
+
+
+# copy to pack
+for folder in os.listdir(root + "/build"):
+    if not folder.startswith(project_name):
         continue
 
-    src = f"{source_path}/bifrost_libs/{folder}"
-    shutil.copytree(src, f"{compounds_path}/{lib_name}", dirs_exist_ok=True)
+    src = f"{root}/build/{folder}"
+    shutil.copytree(src, f"{root}/packs/{project_name}", dirs_exist_ok=True)
     break
 
 
 # Hyuu Library specific post processing
 import json
 
-with open(f"{compounds_path}/Hyuu/lib/OpenCL.json", "r") as f:
+with open(f"{root}/packs/Hyuu/lib/OpenCL.json", "r") as f:
     data = json.load(f)
 for operator in data["operators"]:
     if operator["name"] != "Hyuu::OpenCL::Execute::set_kernel_arg":
@@ -61,5 +67,5 @@ for operator in data["operators"]:
         ]
     })
 
-with open(f"{compounds_path}/Hyuu/lib/OpenCL.json", "w") as f:
+with open(f"{root}/packs/Hyuu/lib/OpenCL.json", "w") as f:
     json.dump(data, f, indent=4)
